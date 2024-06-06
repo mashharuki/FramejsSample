f74d80d1ddd0834ce55fc39b5bddbb48089f3041
mashharuki <avp.104.106.107@ae.auone-net.jp>
2024-06-06 23:00:51 +0900

    chore: Update npm dependencies and add viem package

--- my-frame2/app/frames/route.tsx
+++ my-frame2/app/frames/route.tsx
@@ -1,17 +1,9 @@
 /* eslint-disable react/jsx-key */
-import { getTokenUrl } from 'frames.js';
 import { Button } from 'frames.js/next';
 import { frames } from "./frames";
  
 const handler = frames(async (ctx) => {
  
-  const clickedButtonIndex = ctx.pressedButton;
-  const currentState = ctx.state;
-  
-  console.log("currentState", currentState)
-  console.log("ctx.pressedButton", ctx.pressedButton?.action)
-  console.log("ctx.message", ctx)
-
   const count = ctx.message
     ? ctx.searchParams.op === "+"
       ? ctx.state.count + 1
@@ -41,15 +33,8 @@ const handler = frames(async (ctx) => {
       <Button action="link" target={"https://unchain-tech.github.io/UNCHAIN-projects-feature/"}>
         External
       </Button>,
-      <Button
-        action="mint"
-        target={getTokenUrl({
-          address: "0x060f3edd18c47f59bd23d063bbeb9aa4a8fec6df",
-          tokenId: "123",
-          chainId: 7777777,
-        })}
-      >
-        Mint
+      <Button action="tx" target="/frames/txdata" post_url="/frames">
+        Buy a unit
       </Button>,
     ],
     state: { count: count },
---
+++ my-frame2/app/frames/txdata/route.tsx
@@ -0,0 +1,46 @@
+import { transaction } from "frames.js/core";
+import { Abi, createPublicClient, encodeFunctionData, getContract, http } from "viem";
+import { optimismSepolia } from "viem/chains";
+import { frames } from "../frames";
+import { myTokenABI } from "./contracts/myToken";
+ 
+export const POST = frames(async (ctx) => {
+  if (!ctx?.message) {
+    throw new Error("Invalid frame message");
+  }
+
+  const userAddress = ctx.message.connectedAddress;
+  
+  console.log("userAddress", userAddress)
+
+  // create calldata
+  const calldata = encodeFunctionData({
+    abi: myTokenABI,
+    functionName: "mint",
+    args: [userAddress, 100] as const,
+  });
+
+  const publicClient = createPublicClient({
+    chain: optimismSepolia,
+    transport: http(),
+  });
+
+  const MYTOKEN_ADDRESS = "0xe65C3cFDc4f79e36343cA2e79f17Ce8338095F4c";
+
+  const myToken = getContract({
+    address: MYTOKEN_ADDRESS,
+    abi: myTokenABI,
+    client: publicClient,
+  });
+
+  return transaction({
+    chainId: "eip155:11155420", // OP Sepolia
+    method: "eth_sendTransaction", // sendTransaction
+    params: {
+      abi: myTokenABI as Abi,
+      to: MYTOKEN_ADDRESS,
+      data: calldata,
+      value: "0"
+    },
+  });
+});
